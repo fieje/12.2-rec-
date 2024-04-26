25586e901a22a0a005b8e876400125570f4c0e8a
jack <144657795+fieje@users.noreply.github.com>
2024-04-26 20:58:14 +0300

    Add files via upload

---
+++ 12.2(rec)/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class ListNode
+{
+    public int Value { get; set; }
+    public ListNode Next { get; set; }
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        Value = val;
+        Next = next;
+    }
+}
+
+public class Solution
+{
+    public static void SplitList(ListNode head, out ListNode positiveList, out ListNode negativeList)
+    {
+        positiveList = null;
+        negativeList = null;
+        SplitListRecursive(head, out positiveList, out negativeList);
+    }
+
+    private static void SplitListRecursive(ListNode head, out ListNode positiveList, out ListNode negativeList)
+    {
+        if (head == null)
+        {
+            positiveList = null;
+            negativeList = null;
+            return;
+        }
+
+        SplitListRecursive(head.Next, out positiveList, out negativeList);
+
+        if (head.Value >= 0)
+        {
+            positiveList = new ListNode(head.Value, positiveList);
+        }
+        else
+        {
+            negativeList = new ListNode(head.Value, negativeList);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListNode head = new ListNode(1);
+        head.Next = new ListNode(-2);
+        head.Next.Next = new ListNode(3);
+        head.Next.Next.Next = new ListNode(-4);
+        head.Next.Next.Next.Next = new ListNode(5);
+
+        Console.WriteLine("Початковий список:");
+        PrintList(head);
+        Console.WriteLine();
+
+        ListNode positiveList;
+        ListNode negativeList;
+        Solution.SplitList(head, out positiveList, out negativeList);
+
+        Console.WriteLine("Позитивний список:");
+        PrintList(positiveList);
+
+        Console.WriteLine("Негативний список:");
+        PrintList(negativeList);
+
+        Console.ReadLine();
+    }
+
+    static void PrintList(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.Value + " ");
+            head = head.Next;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program12_2Rec;
+
+namespace Program12_2Rec.Tests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void TestSplitList_PositiveAndNegativeListsAreCorrect()
+        {
+            ListNode head = new ListNode(1,
+                new ListNode(-2,
+                    new ListNode(3,
+                        new ListNode(-4,
+                            new ListNode(5)
+                        )
+                    )
+                )
+            );
+
+            ListNode positiveList;
+            ListNode negativeList;
+
+            Solution.SplitList(head, out positiveList, out negativeList);
+
+            Assert.AreEqual("1 3 5", ConvertToString(positiveList), "Positive list is not correct");
+            Assert.AreEqual("-2 -4", ConvertToString(negativeList), "Negative list is not correct");
+        }
+
+        private string ConvertToString(ListNode head)
+        {
+            string result = "";
+            while (head != null)
+            {
+                result += head.Value + " ";
+                head = head.Next;
+            }
+            return result.Trim();
+        }
+    }
+}
